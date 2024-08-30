074459f2528fc320f9c8ab28c8dae5e116f32113
Darius K <darius.kaya@gmail.com>
2024-08-30 21:36:45 +0200

    almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ import Car from "@/../public/car.svg"
 import Flower from "@/../public/flower.svg"
 import Cabin from "@/../public/cabin.svg"
 import Card from "@/components/Card";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+import NewGameBtn from "@/components/NewGameBtn";
 
 // type CardsArrayProps = {
 //     id: string
@@ -35,49 +36,102 @@ export default function Home() {
     const [cards, setCards] = useState<string[]>(generateDeck())
     const [flipped, setFlipped] = useState<number[]>([])
     const [solved, setSolved] = useState<number[]>([])
+    const [moves, setMoves] = useState<number>(0)
+    const [highscore, setHighscore] = useState<number>(0)
+    const [win, setWin] = useState<boolean>(false)
 
     useEffect(() => {
         const checkForMatch = () => {
             const [first, second] = flipped;
-    
-            if(cards[first] === cards[second]) {
+
+            if (cards[first] === cards[second]) {
                 setSolved([...solved, ...flipped]);
             }
             setFlipped([])
         }
 
-        if(flipped.length === 2) {
+        if (flipped.length === 2) {
             setTimeout(() => {
                 checkForMatch()
             }, 1000)
         }
+
+        console.log(solved.length);
     }, [cards, flipped, solved])
 
     const handleClick = (index: number) => {
         if (!flipped.includes(index) && flipped.length < 2) {
             setFlipped([...flipped, index])
         }
+        if (solved.length < 11)
+            setMoves(moves + 1)
     }
 
+    const newRound = () => {
+        setSolved([])
+        setMoves(0)
+        setCards(generateDeck())
+        setFlipped([])
+        setWin(false)
+    }
+
+    useEffect(() => {
+        if (solved.length === 12) {
+            setWin(true)
+        }
+    }, [solved])
+    useEffect(() => {
+        console.log("win?" + win);
+    }, [win])
+
+    useEffect(() => {
+        const currentHighscore = localStorage.getItem('highscore');
+        // if(currentHighscore)
+        // localStorage.setItem('highscore', '0');
+        setHighscore(parseInt(currentHighscore ?? '0', 10)); // Use nullish coalescing to provide a default value
+
+
+    }, []);
+
+    useEffect(() => {
+        if (win === true) {
+            if (highscore.toString() === '0') {
+                setHighscore(moves)
+                localStorage.setItem('highscore', moves.toString())
+            } else if (moves < highscore) {
+                setHighscore(moves)
+                localStorage.setItem('highscore', moves.toString())
+            }
+        }
+    }, [win])
+
     return (
-        <>
-            <section className="grid grid-cols-4 w-full px-28">
+        <main className="flex flex-col items-center">
+            <section className="grid grid-cols-4 w-full max-w-[1000px] max-h-screen gap-4 my-8">
                 {cards.map((card, index) => {
-                    return <div
+                    return <button disabled={solved.includes(index) || flipped.includes(index) || flipped.length === 2 && true}
                         onClick={() => handleClick(index)}
                         key={index}
-                        className={`h-44 text-8xl font-thin flex justify-center items-center cursor-pointer transform bg-slate-600 p-4 m-8 border-2 border-orange-800 hover:bg-slate-500 transition-transform duration-300 ${flipped.includes(index) || solved.includes(index) ? "rotate-180" : ''}`}>
+                        className={`h-44 w-44 text-8xl font-thin flex justify-center items-center cursor-pointer transform bg-slate-500 hover:bg-slate-600 p-4  border-2 border-orange-800 transition-transform duration-300 ${flipped.includes(index) || solved.includes(index) ? "rotate-180" : ''}`}>
                         {flipped.includes(index) || solved.includes(index) ? (
                             <Image className="rotate-180" src={card} alt="Card" />
                         ) : (
                             '?'
                         )}
-                    </div>
+                    </button>
                 })}
             </section>
-            <section className="bg-slate-500 w-[50%] border-2 border-orange-800 p-10">
-                hello
+            <section className="m-4 bg-slate-500 border-2 border-orange-800 p-10 w-full max-w-[1000px] flex justify-evenly items-center">
+                <div className="flex flex-col justify-center items-center">
+                    <h2 className="text-2xl">Moves:</h2>
+                    <p className={`text-3xl text-orange-900 ${solved.length === 12 && 'text-green-600 text-5xl'}`}>{moves}</p>
+                </div>
+                <NewGameBtn newRound={newRound} />
+                <div className="flex flex-col justify-center items-center">
+                    <h2 className="text-2xl">Highscore:</h2>
+                    <p className={`text-3xl text-green-600`}>{highscore}</p>
+                </div>
             </section>
-        </>
+        </main>
     );
 }
---
+++ src/components/NewGameBtn/index.tsx
@@ -0,0 +1,13 @@
+type newGameBtnProps = {
+    newRound: () => void
+}
+
+const NewGameBtn = ({ newRound }:newGameBtnProps) => {
+    return (
+        <button onClick={newRound} className="text-2xl text-orange-800 border-2 border-orange-800 py-2 px-8 rounded transition bg-slate-700 hover:bg-slate-500 hover:text-black">
+            New Game
+        </button>
+    )
+};
+
+export default NewGameBtn
