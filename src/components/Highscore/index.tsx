d0b6ec5f3c2e147ae647865e46acfa9c7cd8c188
Darius K <darius.kaya@gmail.com>
2024-09-04 20:16:16 +0200

    fixed input maxLength bug

--- src/components/Highscore/index.tsx
+++ src/components/Highscore/index.tsx
@@ -21,7 +21,7 @@ const Highscore = ({ updateNewHighscore }:HighscoreProps) => {
         <div data-testid="highscore-popup" className="bg-slate-200 p-10 fixed top-[50%] -translate-y-1/2 flex flex-col items-center shadow-2xl">
             <h2 className="text-3xl mb-4">New Highscore!</h2>
             {/* <label htmlFor="name">Enter name</label> */}
-            <input onChange={handleChange} className="mb-4" data-testid="input" placeholder="Enter your name ..." id="name" maxLength="15" required/>
+            <input onChange={handleChange} className="mb-4" data-testid="input" placeholder="Enter your name ..." id="name" maxLength={15} required/>
             <button data-testid="highscore-button" onClick={handleClick} className="bg-slate-400 w-36 py-1 hover:bg-slate-500 transition-all">Done</button>
         </div>
     )
